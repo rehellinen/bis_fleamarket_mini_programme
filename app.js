de007072297688ef3b99e526a7adba3522cbbba0
rehellinen <912377791@qq.com>
2018-05-21 15:18:49 +0800

    商品详情页面重构

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ App({
   onLaunch(){
     let token = new Token
     // 检验openID是否存在
-    token.verifyOpenID()
+    // token.verifyOpenID()
     // 检验Token令牌
     token.getTokenFromServer()
   }
--- pages/goods-detail/goods-detail.js
+++ pages/goods-detail/goods-detail.js
@@ -1,101 +1,105 @@
 import {GoodsModel} from '../../model/GoodsModel.js'
+
 let goods = new GoodsModel()
 
 Page({
-  data: {
-    imageUrl: ''
-  },
+    data: {
+        imageUrl: ''
+    },
 
-  onLoad: function (options) {
-    let id = options.id
-    let url = 'oldGoods/'
-    if(wx.getStorageSync('type') == 'shop'){
-      url = 'newGoods/'
-    }
+    onLoad: function (options) {
+        this.data.id = options.id
+        this._loadData()
+    },
 
-    // id为0表示修改
-    if(id != 0){
-      // 加载数据
-      goods.getGoodsDetail(url + id, (res) => {
-        this.setData({
-          id: id,
-          info: res,
-          imageUrl: res.image_id.image_url,
-          imageID: res.image_id.id
-        })
-      })
-    }
-  },
-
-  // 上传图片
-  uploadImage(event){
-    let that = this
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['compressed'],
-      success(res) {
-        wx.uploadFile({
-          url: goods.baseUrl + 'image/goods',
-          filePath: res.tempFilePaths[0],
-          name: 'image',
-          header: {
-            token: wx.getStorageSync('token')
-          },
-          success(data) {
-            let jsonData = JSON.parse(data.data)
-            that.setData({
-              imageID: jsonData.data.image_id,
-              imageUrl: res.tempFilePaths[0]
+    _loadData() {
+        let url = 'oldGoods/'
+        if (wx.getStorageSync('type') == 'shop') {
+            url = 'newGoods/'
+        }
+        // id为0表示添加
+        if (this.data.id != 0) {
+            // 加载数据
+            goods.getGoodsDetail(url + this.data.id, (res) => {
+                this.setData({
+                    id: id,
+                    info: res,
+                    imageUrl: res.image_id.image_url,
+                    imageID: res.image_id.id
+                })
             })
-          }
-        })        
-      }
-    })
-  },
+        }
+    },
 
-  submit(event){
-    let data = event.detail.value
-    data.image_id = this.data.imageID
+    // 上传图片
+    uploadImage(event) {
+        let that = this
+        wx.chooseImage({
+            count: 1,
+            sizeType: ['compressed'],
+            success(res) {
+                wx.uploadFile({
+                    url: goods.baseUrl + 'image/goods',
+                    filePath: res.tempFilePaths[0],
+                    name: 'image',
+                    header: {
+                        token: wx.getStorageSync('token')
+                    },
+                    success(data) {
+                        let jsonData = JSON.parse(data.data)
+                        that.setData({
+                            imageID: jsonData.data.image_id,
+                            imageUrl: res.tempFilePaths[0]
+                        })
+                    }
+                })
+            }
+        })
+    },
 
-    if(this.data.id){
-      // 修改商品信息
-      data.id = this.data.id
-      goods.editGoods(data, (res) => {
-        wx.setStorageSync('newGoods', true)
-        wx.showModal({
-          title: '成功',
-          content: '修改商品信息成功',
-          showCancel: false,
-          success() {
-            wx.navigateBack({})            
-          }          
-        })
-      }, (res) => {
-        wx.showModal({
-          title: '失败',
-          showCancel: false,
-          content: res.message
-        })
-      })
-    }else{
-      // 添加商品
-      goods.addGoods(data, (res) => {
-        wx.setStorageSync('newGoods', true)
-        wx.showModal({
-          title: '成功',
-          showCancel: false,
-          content: '添加商品成功',
-          success() {
-            wx.navigateBack({})
-          }
-        })
-      }, (res) => {
-        wx.showModal({
-          title: '失败',
-          showCancel: false,
-          content: res.message
-        })
-      })
-    }    
-  }
+    submit(event) {
+        let data = event.detail.value
+        data.image_id = this.data.imageID
+
+        if (this.data.id) {
+            // 修改商品信息
+            data.id = this.data.id
+            goods.editGoods(data, (res) => {
+                wx.setStorageSync('newGoods', true)
+                wx.showModal({
+                    title: '成功',
+                    content: '修改商品信息成功',
+                    showCancel: false,
+                    success() {
+                        wx.navigateBack({})
+                    }
+                })
+            }, (res) => {
+                wx.showModal({
+                    title: '失败',
+                    showCancel: false,
+                    content: res.message
+                })
+            })
+        } else {
+            // 添加商品
+            goods.addGoods(data, (res) => {
+                wx.setStorageSync('newGoods', true)
+                wx.showModal({
+                    title: '成功',
+                    showCancel: false,
+                    content: '添加商品成功',
+                    success() {
+                        wx.navigateBack({})
+                    }
+                })
+            }, (res) => {
+                wx.showModal({
+                    title: '失败',
+                    showCancel: false,
+                    content: res.message
+                })
+            })
+        }
+    }
 })
